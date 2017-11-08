702285ebb689c2379bbe3b201bcc106e52518108
harrygreg <30847669+harrygreg@users.noreply.github.com>
2017-11-08 20:34:58 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,4 +12,7 @@ class Curso(models.Model):
 
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
+    
+class Candidato(models.Model):
+    nome = nome = models.CharField(max_length=200)
 
